aa84cbcebaca2e0a53f64fb99d9b0ea78951d2ce
victor george <victorvg17@gmail.com>
2020-05-29 18:08:52 +0200

    solution found

--- ex2/templates/task2/ball_trajectory.m
+++ ex2/templates/task2/ball_trajectory.m
@@ -16,10 +16,9 @@ P = full(pos_t(v_start));
 % subject to X(7)^2 + X(8)^2 <= v_bar^2;
 v_bar = 15;
 p0 = [0, 0, 10, 0];
-p = SX.sym('p', 1, 4);
 v = SX.sym('v', 1, 4);
-k = [v, p];
-f = full(pos_t(v));
+p = full(pos_t(v));
+f = (p(1) - p(3))^2 + (p(2) - p(4))^2;
 g = [p(2);
      p(4);
      v(1)^2 + v(2)^2;
@@ -27,11 +26,11 @@ g = [p(2);
  
 
 % Create IPOPT solver object
-nlp = struct('x', k, 'f', f, 'g', g);
+nlp = struct('x', v, 'f', f, 'g', g);
 solver = nlpsol('solver', 'ipopt', nlp);
 
 % Solve the NLP
-res = solver('k0', [[0, 0, 0, 0],[0, 0, 10, 0]],...
+res = solver('x0', [0, 0, 0, 0],...
              'lbx', -inf,...
              'ubx', inf,...
              'lbg', [0; 0; -inf; -inf],...
@@ -39,6 +38,6 @@ res = solver('k0', [[0, 0, 0, 0],[0, 0, 10, 0]],...
          
 % Print the solution
 f_opt_ball = full(res.f);
-k_opt_ball = full(res.k);
+x_opt_ball = full(res.x);
 lam_x_opt_ball = full(res.lam_x);
 lam_g_opt_ball = full(res.lam_g);
