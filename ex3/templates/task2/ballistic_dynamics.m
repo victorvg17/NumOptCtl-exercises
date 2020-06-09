0459d53d423bea12cc33c1d136306c9f4fbfe93f
victor george <victorvg17@gmail.com>
2020-06-09 16:12:40 +0200

    solution from tutor for ex3

---
+++ ex3/templates/task2/ballistic_dynamics.m
@@ -0,0 +1,13 @@
+function [ dx ] = ballistic_dynamics( x ) 
+
+w = 2;
+d = .1;
+g = 9.81;
+
+v = x(3:4);
+dnorm = norm( v - [w;0]);
+
+dx  = [ v(1);v(2);
+        -(v(1) - w) * dnorm  * d ;
+        -v(2)  * dnorm * d - g];
+end
